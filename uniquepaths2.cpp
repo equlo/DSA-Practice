49fee1d586b9ce5e54345cdda795daa1b4906c01
Ank_gojo <116424026+equlo@users.noreply.github.com>
2026-05-13 17:44:44 +0530

    Add files via upload

---
+++ uniquepaths2.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        // If starting cell has obstacle
+        if (obstacleGrid[0][0] == 1)
+            return 0;
+
+        dp[0][0] = 1;
+
+        // Fill first column
+        for (int i = 1; i < m; i++) {
+            if (obstacleGrid[i][0] == 0)
+                dp[i][0] = dp[i - 1][0];
+        }
+
+        // Fill first row
+        for (int j = 1; j < n; j++) {
+            if (obstacleGrid[0][j] == 0)
+                dp[0][j] = dp[0][j - 1];
+        }
+
+        // Fill remaining cells
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+};
+
+int main() {
+
+    vector<vector<int>> obstacleGrid = {
+        {0,0,0},
+        {0,1,0},
+        {0,0,0}
+    };
+
+    Solution obj;
+
+    cout << "Unique Paths: "
+         << obj.uniquePathsWithObstacles(obstacleGrid);
+
+    return 0;
+}
