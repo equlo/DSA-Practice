37d7f95076bb7386df48aef82061a66269ac8ee4
Ank_gojo <116424026+equlo@users.noreply.github.com>
2026-06-17 12:29:48 +0530

    Add files via upload

---
+++ ToeplitzMatrix.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            if (matrix[i][j] != matrix[i - 1][j - 1]) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 2, 3, 4},
+        {5, 1, 2, 3},
+        {9, 5, 1, 2}
+    };
+
+    cout << (isToeplitzMatrix(matrix) ? "true" : "false") << endl;
+
+    return 0;
+}
