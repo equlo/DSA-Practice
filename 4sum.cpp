61de726fdff00e4e2b45360fd5ada0e4ead9fb85
Ank_gojo <116424026+equlo@users.noreply.github.com>
2026-07-03 22:23:36 +0530

    Add files via upload

---
+++ 4sum.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    vector<vector<int>> ans;
+    int n = nums.size();
+
+    sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < n - 3; i++) {
+
+        // Skip duplicate values for i
+        if (i > 0 && nums[i] == nums[i - 1])
+            continue;
+
+        for (int j = i + 1; j < n - 2; j++) {
+
+            // Skip duplicate values for j
+            if (j > i + 1 && nums[j] == nums[j - 1])
+                continue;
+
+            int left = j + 1;
+            int right = n - 1;
+
+            while (left < right) {
+
+                long long sum = (long long)nums[i] + nums[j] +
+                                nums[left] + nums[right];
+
+                if (sum == target) {
+                    ans.push_back({nums[i], nums[j], nums[left], nums[right]});
+
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+
+                    while (left < right && nums[right] == nums[right + 1])
+                        right--;
+                }
+                else if (sum < target) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
+            }
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {1, 0, -1, 0, -2, 2};
+    int target = 0;
+
+    vector<vector<int>> ans = fourSum(nums, target);
+
+    cout << "Quadruplets are:\n";
+    for (auto &quad : ans) {
+        cout << "[ ";
+        for (int x : quad)
+            cout << x << " ";
+        cout << "]\n";
+    }
+
+    return 0;
+}
